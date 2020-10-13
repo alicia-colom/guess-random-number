26d52d01b8ce5f8864bd30ab86ce0c27773d2bb7
alicia-colom <aliciacolom@gmail.com>
2020-10-13 11:27:02 +0200

    Add return in HTML instead cosole.log

--- main.js
+++ main.js
@@ -6,39 +6,37 @@
 // Función de número aleatorio:
 
 function getRandomNumber(max) {
-return Math.ceil(Math.random() * max);
-};
+	return Math.ceil(Math.random() * max);
+}
 
 const randomNumber = getRandomNumber(100);
 console.log("El número aleatorio es " + randomNumber);
 
-let tryNumber = (document.querySelector(".js-try").innerHTML = 0);
-console.log(tryNumber);
-
+let tryNumber = document.querySelector(".js-try");
+tryNumber.innerHTML = 0;
 
 // Función envío de número
 function handleSendNumber() {
 	const userNumber = parseInt(document.querySelector(".js-textArea").value);
 	console.log("El número introducido por el usuario es " + userNumber);
 
-	/*   const clue = document.querySelector(".js-clue").innerHTML;
-  const newClue = "Esto es una nueva pista";
-	console.log(newClue); */
+	const clue = document.querySelector(".js-clue");
 
 	if (userNumber > 100) {
-		console.log("El número debe estar entre 1 y 100.");
+		clue.innerHTML = "El número debe estar entre 1 y 100.";
+	} else if (userNumber < 1) {
+    clue.innerHTML = "El número debe estar entre 1 y 100.";
 	} else if (userNumber > randomNumber) {
-		console.log("Demasiado alto.");
+    clue.innerHTML = "Demasiado alto.";
 	} else if (userNumber < randomNumber) {
-		console.log("Demasiado bajo.");
+    clue.innerHTML = "Demasiado bajo.";
 	} else if (userNumber === randomNumber) {
-		console.log("¡Has ganado!");
+    clue.innerHTML = "¡Has ganado!";
 	} else {
-		console.log("El número debe estar entre 1 y 100.");
-  }
-  
-  tryNumber = ++tryNumber;
-	console.log(tryNumber);
+    clue.innerHTML = "El número debe estar entre 1 y 100.";
+	}
+
+	tryNumber.innerHTML = ++tryNumber.innerHTML;
 }
 
 const button = document.querySelector(".js-btn");
